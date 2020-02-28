3ea9b3bc3d3aaa6b8ff348e1ba75d878f1dbc132
Cyrus Z Goodarzi <cmg1418@skee-304-007.ee.ic.ac.uk>
2020-02-28 11:26:36 +0000

    branch notes

--- include/ast.hpp
+++ include/ast.hpp
@@ -9,18 +9,21 @@ class ast_node{
   public:
     std::string node_type;
     std::string value;
+    std::vector<std::string> branch_notes;
     std::vector<ast_node*> branches;
 
-    ast_node(std::string _node_type, std::string _value, std::vector<ast_node*> _branches){
+    ast_node(std::string _node_type, std::string _value, std::vector<ast_node*> _branches, std::vector<std::string> _branch_notes){
       node_type = _node_type;
       value = _value;
       branches = _branches;
+      branch_notes = _branch_notes;
     }
-    
+
     ast_node(std::string _node_type, std::string _value){
       node_type = _node_type;
       value = _value;
       branches = {};
+      branch_notes = {};
     }
     ~ast_node();
 };
