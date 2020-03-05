3fd0db44c6b434a2e24ff51fce61af11b3ad2386
Cyrus Z Goodarzi <cmg1418@skee-304-019.ee.ic.ac.uk>
2020-03-05 12:48:37 +0000

    need to handle global variables in python using some sort of basic symbol table

--- src/print_canonical.cpp
+++ src/print_canonical.cpp
@@ -6,5 +6,11 @@ int main()
     int tab_count = 0;
     ast_node *ast=parseAST();
     ast->print_python(tab_count);
+
+    std::cout<<"if __name__ == \"__main__\":"<<std::endl;
+    std::cout<<"\timport sys"<<std::endl;
+    std::cout<<"\tret=main()"<<std::endl;
+    std::cout<<"\tsys.exit(ret)"<<std::endl;
+
     return 0;
 }
