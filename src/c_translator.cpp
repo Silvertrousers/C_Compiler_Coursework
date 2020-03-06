1f413b00fd4a2abe3f28005469efcc8cd3bf8708
Cyrus Z Goodarzi <cmg1418@skee-304-007.ee.ic.ac.uk>
2020-03-06 12:42:48 +0000

    quality oflife changes

--- src/c_translator.cpp
+++ src/c_translator.cpp
@@ -10,14 +10,11 @@ int main()
     ast_node *ast=parseAST();
     ast->print_python(tab_count, sym_tab);
 
+    std::cout<<std::endl;
     std::cout<<"if __name__ == \"__main__\":"<<std::endl;
     std::cout<<"\timport sys"<<std::endl;
     std::cout<<"\tret=main()"<<std::endl;
     std::cout<<"\tsys.exit(ret)"<<std::endl;
 
-    std::cout<<std::endl;
-    for(int i=0;i<(sym_tab.symbols).size();i++){
-      std::cout<<"\t"<<sym_tab.symbols[i]<<", "<<std::endl;
-    }
     return 0;
 }
